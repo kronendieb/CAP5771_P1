e84ece27ed08931fc70ee9f452fe81ca81acc451
kronendieb <casantiago.personal@gmail.com>
2024-10-22 20:54:19 -0400

    Added the info file generation

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+from functools import total_ordering
 from mlxtend import frequent_patterns
 import pandas as pd
 from mlxtend.preprocessing import TransactionEncoder
 from mlxtend.frequent_patterns import apriori, association_rules
 import argparse
+import time
 
 # Parse the arguments from the command line
 def parse_args():
@@ -38,6 +40,8 @@ def apriori_algorithm(transactions, minsuppc):
     return frequent_itemsets
 
 def generate_rules(frequent_itemsets, minconf):
+    if minconf < 0:
+        return
     rules = association_rules(frequent_itemsets, metric="confidence", min_threshold=minconf)
     return rules
 
@@ -51,6 +55,8 @@ def frequent_itemsets_to_file(frequent_itemsets, transactions, output_file):
             file.write(f"{itemset}|{support_count}\n")
 
 def frequent_rules_to_file(frequent_rules, transactions, output_file):
+    if frequent_rules is None:
+        return
     total_transactions = len(transactions)
 
     with open(output_file, 'w') as file:
@@ -61,12 +67,45 @@ def frequent_rules_to_file(frequent_rules, transactions, output_file):
             confidence = int(row['confidence'])
             file.write(f"{lhs}|{rhs}|{support_count}|{confidence}\n")
 
-def longest_transaction(transactions):
-
+def get_longest_transaction(transactions):
     lengths = transactions.sum(axis=1)
     longest = lengths.max()
     return longest
 
+def get_itemset_lengths(frequent_itemsets):
+    frequent_itemsets['length'] = frequent_itemsets['itemsets'].apply(lambda x: len(x))
+
+    itemsets_by_size = frequent_itemsets['length'].value_counts().sort_index()
+    return itemsets_by_size
+
+def get_rule_with_highest_confidence(frequent_rules):
+    return frequent_rules.loc[frequent_rules['confidence'].idxmax()]
+
+def info_to_file(args, transactions, frequent_itemsets, frequent_rules, times, output_file):
+    with open(output_file, 'w') as file:
+        total_items = len(transactions.columns)
+        total_transactions = len(transactions)
+        longest_transaction = get_longest_transaction(transactions)
+        itemsets_by_size = get_itemset_lengths(frequent_itemsets)
+        total_frequent_itemsets = len(frequent_itemsets)
+        total_frequent_rules = len(frequent_rules)
+        # Highest Confidence Rule
+        hcr = get_rule_with_highest_confidence(frequent_rules)
+
+        # Output the args
+        file.write(f"minsuppc:{args.minsuppc}\nminconf:{args.minconf}\ninput file:{args.filename}\n")
+        # Output the transaction totals:
+        file.write(f"Number of items:{total_items}\nNumber of transactions:{total_transactions}\nThe length of the longest transaction:{longest_transaction}\n")
+
+        # Output itemeset values
+        for i, val in enumerate(itemsets_by_size):
+            file.write(f"Number of frequent {i+1}-itemsets:{val}\n")
+        file.write(f"Total number of frequent itemsets:{total_frequent_itemsets}\n")
+
+        # Output rules values:
+        file.write(f"Number of high-confidence rules:{total_frequent_rules}\n")
+        file.write(f"The rule with the highest confidence:{' '.join(map(str,hcr['antecedents']))}|{' '.join(map(str,hcr['consequents']))}|{int(hcr['support']*total_transactions)}|{hcr['confidence']:.4f}\n")
+        file.write(f"Time in seconds to find the frequent itemsets:{times[0]}\nTime in seconds to find the confident rules:{times[1]}")
 
 def main():
     args = parse_args()
@@ -78,17 +117,26 @@ def main():
     te = TransactionEncoder()
     te_arr = te.fit(data).transform(data)
     df = pd.DataFrame(te_arr, columns=te.columns_)
-    print(longest_transaction(df))
 
+    times = [0.0, 0.0]
 
+    print("Finding frequent itemsets...")
+    start_time = time.time()
     frequent_itemsets = apriori_algorithm(df, min_support_count)
-    print("Frequent Itemsets:\n", frequent_itemsets)
+    end_time = time.time()
+    times[0] = end_time - start_time
+    print(f"Done! Time taken to find frequent itemsets: {times[0]:.4f} seconds")
 
+    print("Finding rules with high confidence...")
+    start_time = time.time()
     frequent_rules = generate_rules(frequent_itemsets, min_confidence)
-    print("Frequent Rules:\n", frequent_rules)
+    end_time = time.time()
+    times[1] = end_time - start_time
+    print(f"Done! Time taken to find frequent itemsets: {times[1]:.4f} seconds")
 
     frequent_itemsets_to_file(frequent_itemsets, df, "items_01.txt")
     frequent_rules_to_file(frequent_rules, df, "rules_01.txt")
+    info_to_file(args, df, frequent_itemsets, frequent_rules, times, "info_01.txt")
 
     return 0
 
